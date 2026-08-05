f0921c8c99859a27ff5cb7cce09cf8475f832cfc
servius <servius@darksailor.dev>
2026-08-05 13:57:24 +0530

    chore: Cargo fmt

--- mnn-bridge/src/lib.rs
+++ mnn-bridge/src/lib.rs
@@ -1,10 +1,20 @@
 #[cfg(not(any(
-     all(feature = "ndarray_0_15", not(any(feature = "ndarray_0_16", feature = "ndarray_0_17"))),
-     all(feature = "ndarray_0_16", not(any(feature = "ndarray_0_15", feature = "ndarray_0_17"))),
-     all(feature = "ndarray_0_17", not(any(feature = "ndarray_0_15", feature = "ndarray_0_16"))),
+    all(
+        feature = "ndarray_0_15",
+        not(any(feature = "ndarray_0_16", feature = "ndarray_0_17"))
+    ),
+    all(
+        feature = "ndarray_0_16",
+        not(any(feature = "ndarray_0_15", feature = "ndarray_0_17"))
+    ),
+    all(
+        feature = "ndarray_0_17",
+        not(any(feature = "ndarray_0_15", feature = "ndarray_0_16"))
+    ),
 )))]
-compile_error!("Only one of `ndarray_0_15`, `ndarray_0_16`, `ndarray` (`ndarray_0_17`) must be enabled");
-
+compile_error!(
+    "Only one of `ndarray_0_15`, `ndarray_0_16`, `ndarray` (`ndarray_0_17`) must be enabled"
+);
 
 #[cfg(feature = "ndarray_0_17")]
 pub mod ndarray_0_17 {
